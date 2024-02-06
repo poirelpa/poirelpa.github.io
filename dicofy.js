b01d15cba609140b8e3fa6e853f801a80763c9ee
Paul Poirel <poirelpa@gmail.com>
2024-02-06 16:56:47 +0100

    Create dicofy.js

---
+++ dicofy.js
@@ -0,0 +1,14 @@
+function dicofy(el, url){
+  let text = el.innerHTML.split(' ')
+  el.replaceChildren();
+  for(let i=0;i<text.length;i++){
+    let a = document.createElement('a');
+    a.setAttribute('href',url.replace('?',text[i].replace(/[\W]/gi,' ')));
+    a.append(text[i]);
+    el.appendChild(a);
+    el.append(' ');
+  }
+}
+
+//example :
+// dicofy(document.getElementById("verso"),'http://wordreference.com/defr/?')
