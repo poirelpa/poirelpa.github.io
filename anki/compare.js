12baea8a58789b3a4f96b3df7bba01d70d862081
Paul Poirel <poirelpa@gmail.com>
2025-12-10 18:25:52 +0100

    Update compare.js

--- anki/compare.js
+++ anki/compare.js
@@ -30,8 +30,10 @@ function betterCompare(){
 			answerSpan.classList.add('typeGood');
 		}
 		if(entrySpan.classList.contains('typeMissed')){
-		    let code = answer.charCodeAt(1).toString(16).toUpperCase();
+		    let code = answer.charCodeAt(0).toString(16).toUpperCase();
 			document.querySelector('span#typearrow').innerText = code;
+			
+			document.querySelector('span#typearrow').innerText = code + ' ' +answer.charCodeAt(1).toString(16).toUpperCase();
 		}
 		// ignore arabic heh vs teh marbuta
 		if(entrySpan.classList.contains('typeBad') && answer=='ة' && entry=='ه'){
